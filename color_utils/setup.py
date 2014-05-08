c9d98c6b710586a801a9b4fed1cf8d8faa3456ca
jon <jonmer85@gmail.com>
2014-05-08 16:17:41 -0400

    Added setup.py

---
+++ color_utils/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+tests_require = [
+    'Django>=1.3,<1.7',
+]
+
+setup(
+    name='django-color-utils',
+    author='Tim Church',
+    url='https://github.com/timchurch/django-color-utils',
+    zip_safe=False,
+)
